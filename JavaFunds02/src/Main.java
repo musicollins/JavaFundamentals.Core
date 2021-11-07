446b88114307b36a2e74fc3cb65e7ee8149c03c7
Rafael <rafaelmilanes91@gmail.com>
2021-11-07 09:05:59 +0100

    Java Fundamentals

--- JavaFunds02/src/Main.java
+++ JavaFunds02/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("01 Java Fundamentals I/O Streams and Files");
+
+        /*Streams.
+            Streams Errors and Cleanup.
+            Chaining Streams.
+            File and Buffered Streams.
+            Accessing files with java.nio.file package
+            File Systems
+            Creating and working with ZIP file systems
+         */
     }
 }
